f241f89965649077040ad5d4f161e6d850f4467e
ransom <auvergnerw@gmail.com>
2015-08-23 02:33:56 -0500

    wiring

--- catalog/capp/urls.py
+++ catalog/capp/urls.py
@@ -4,9 +4,20 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    # todo: item titles and category names cannot be 'item'
+    url(r'^item/new$',
+        views.item_add,
+        name='item_add'),
     url(r'^(?P<category_name>.+)/items$',
         views.items_list,
         name='items_list'),
+    url(r'^(?P<item_title>.+)/edit$',
+        views.item_edit,
+        name='item_edit'),
+    url(r'^(?P<item_title>.+)/delete$',
+        views.item_delete,
+        name='item_delete'),
+    # todo: item titles cannot be 'items', 'edit', or 'delete'
     url(r'^(?P<category_name>.+)/(?P<item_title>.+)$',
         views.item_detail,
         name='item_detail')
--- catalog/capp/views.py
+++ catalog/capp/views.py
@@ -2,6 +2,7 @@ from pdb import set_trace as st
 
 from django.shortcuts import render
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 
 from .models import Category
 from .models import Item
@@ -40,3 +41,29 @@ def item_detail(request, category_name, item_title):
     return render(request, TEMPLATE_DIR + '/item.html',
                   RequestContext(request,
                                  {'item': item}))
+
+
+@login_required
+def item_add(request):
+    return render(request, TEMPLATE_DIR + '/item_add_edit.html',
+                  RequestContext(request,
+                                 {'action': "Add"}))
+
+
+# todo: check users's ownership before allowing to modify
+#       do not allow deleting items w/o associated users
+#       ... perhaps use a decorator function that takes
+#           a test function as an argument
+
+@login_required
+def item_edit(request, item_title):
+    return render(request, TEMPLATE_DIR + '/item_add_edit.html',
+                  RequestContext(request,
+                                 {'action': "Edit"}))
+
+
+@login_required
+def item_delete(request, item_title):
+    return render(request, TEMPLATE_DIR + '/item_delete.html',
+                  RequestContext(request,
+                                 {}))
